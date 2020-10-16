8c4f2dead7481dd0b595355ea27b9a269795fc3c
gqzhang5 <gqzhang5@iflytek.com>
2020-10-16 15:37:04 +0800

    feat: 完善解析字幕脚本
    affects: auto-add-subtitle

--- packages/auto-add-subtitle/src/index.ts
+++ packages/auto-add-subtitle/src/index.ts
@@ -4,10 +4,14 @@ import {
   changeFormat,
   ConcurrentTasks,
   getClosestNodeModulesPath,
+  handleError,
   makeMap,
+  move,
   readdir,
   sliceMediaByDuration,
+  writeFile,
 } from './utils';
+import { mergeSrtFiles } from './utils/subtitles';
 import { Veed } from './veed-auto-add-title';
 
 const videoDir = path.resolve(
@@ -15,31 +19,35 @@ const videoDir = path.resolve(
   '.cache/videos',
 );
 
-async function prepareMp3Files() {
-  const files = await readdir(videoDir);
-
-  await changeFormat(
-    files.map(file => path.resolve(videoDir, file)),
-    'mp3',
-  );
+function isFile(file: string) {
+  return /\.\w+/.test(file);
 }
 
-async function checkMp3Files() {
-  const files = await readdir(videoDir);
+async function prepareMp3Files() {
+  const files = await readdir(videoDir).catch(handleError);
+  if (!files) {
+    return;
+  }
+
+  await changeFormat(
+    files.filter(isFile).map(file => path.resolve(videoDir, file)),
+    'mp3',
+  );
 
   await new ConcurrentTasks(
-    files
-      .filter(file => file.endsWith('mp3'))
-      .map(file => async () => {
-        await sliceMediaByDuration(path.resolve(videoDir, file), 6 * 60);
-      }),
+    files.filter(isFile).map(file => async () => {
+      await sliceMediaByDuration(
+        path.resolve(videoDir, file.replace(/\.\w$/, '.mp3')),
+        6 * 60,
+      );
+    }),
   ).run();
 }
 
 async function parseSubtitle() {
-  const parsedFiles = await readdir(
-    path.resolve(videoDir, 'parsed'),
-  ).catch(() => {});
+  const parsedFiles = await readdir(path.resolve(videoDir, 'parsed')).catch(
+    handleError,
+  );
   const hasParsed = makeMap(
     (parsedFiles || []).map(file =>
       path.parse(file).name.replace('default_Project Name_', ''),
@@ -60,10 +68,65 @@ async function parseSubtitle() {
   );
 }
 
+async function mergeSrtChunks() {
+  const files = await readdir(path.resolve(videoDir, 'parsed')).catch(
+    handleError,
+  );
+  if (!files) {
+    return;
+  }
+
+  const groupedFiles = files
+    .filter(file => /chunks_\d+/.test(file))
+    .map(file => path.resolve(videoDir, `parsed/${file}`))
+    .reduce((acc: Record<string, string[]>, cur) => {
+      const [, num] = cur.match(/_([\d.]+)_chunks/) || [cur, 0];
+      if (!acc[num]) {
+        acc[num] = [cur];
+      } else {
+        acc[num].push(cur);
+      }
+      return acc;
+    }, {});
+
+  const result = await Promise.all(
+    Object.keys(groupedFiles).map(key => mergeSrtFiles(groupedFiles[key])),
+  );
+
+  await Promise.all(
+    Object.keys(groupedFiles).map((key, i) =>
+      writeFile(path.resolve(videoDir, `parsed/${key}.srt`), result[i]),
+    ),
+  );
+}
+
+async function renameSrtFiles() {
+  const files = await readdir(path.resolve(videoDir, 'parsed')).catch(
+    handleError,
+  );
+  if (!files) {
+    return;
+  }
+
+  await Promise.all(
+    files
+      .filter(file => /default_Project Name_(?:[\d.]+)\.mp3\.srt/.test(file))
+      .map(file => path.resolve(videoDir, `parsed/${file}`))
+      .map(file =>
+        move(
+          file,
+          file.replace('default_Project Name_', '').replace('.mp3', ''),
+        ),
+      ),
+  );
+}
+
 async function main() {
-  // await prepareMp3Files();
-  // await checkMp3Files();
+  await prepareMp3Files();
   await parseSubtitle();
+
+  await mergeSrtChunks();
+  await renameSrtFiles();
 }
 
 main();
