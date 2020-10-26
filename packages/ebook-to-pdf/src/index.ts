2c2695d5a861041bd846c43737cdffbb0f4f71f2
gqzhang5 <gqzhang5@iflytek.com>
2020-10-26 14:29:37 +0800

    feat: 过滤已经转过的pdf文件
    affects: ebook-to-pdf

--- packages/ebook-to-pdf/src/index.ts
+++ packages/ebook-to-pdf/src/index.ts
@@ -1,8 +1,16 @@
-import { ConcurrentTasks, execAsync, readdir } from 'zgq-shared';
+import { ConcurrentTasks, execAsync, makeMap, readdir } from 'zgq-shared';
 import path from 'path';
 
 function isEbook(file: string) {
   return /\.e\w+$/.test(file);
+}
+
+function isPdf(file: string) {
+  return /\.pdf$/.test(file);
+}
+
+function parseName(file: string) {
+  return path.parse(file).name;
 }
 
 export async function convert2Pdf(filePath: string) {
@@ -11,10 +19,12 @@ export async function convert2Pdf(filePath: string) {
   }
 
   const files = await readdir(filePath);
+  const hasConverted = makeMap(files.filter(isPdf).map(parseName));
 
   await new ConcurrentTasks(
     (files || [])
       .filter(isEbook)
+      .filter(file => !hasConverted(parseName(file)))
       .map(file => path.resolve(filePath, file))
       .map(file => async () => {
         await execAsync(
