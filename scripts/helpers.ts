b4e7aa8cc30208bfe46e9a6f07419c58cf6adb25
gqzhang5 <gqzhang5@iflytek.com>
2020-10-30 14:11:58 +0800

    chore: pack脚本优化

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,5 +1,12 @@
-import { readFile, withCache } from '../packages/shared/src';
+import {
+  readFile,
+  withCache,
+  readdir,
+  ConcurrentTasks,
+  writeFile,
+} from '../packages/shared/src';
 import fs from 'fs';
+import path from 'path';
 
 export async function getPreviousFileContent(
   filePath: string,
@@ -12,4 +19,32 @@ export async function getPreviousFileContent(
     path => readFile(path, { encoding: 'utf-8' }),
     1000 * 60 * 5,
   )(filePath);
+}
+
+export async function updateMarkdownContent(
+  transform: (md: string) => string = x => x,
+) {
+  const packagesDir = path.resolve(__dirname, '../packages/');
+  const packages = await readdir(packagesDir);
+
+  await new ConcurrentTasks(
+    packages.map(packagePath => async () => {
+      const files = await readdir(path.resolve(packagesDir, packagePath));
+      await Promise.all(
+        files.filter(isMarkdown).map(async file => {
+          const readmePath = path.resolve(packagesDir, packagePath, file);
+          const md = await getPreviousFileContent(readmePath);
+          if (!md) {
+            return;
+          }
+
+          await writeFile(readmePath, transform(md));
+        }),
+      );
+    }),
+  ).run();
+}
+
+function isMarkdown(file: string) {
+  return /\.md$/.test(file);
 }
--- scripts/postpack.ts
+++ scripts/postpack.ts
@@ -1,21 +1,5 @@
-import path from 'path';
-import { ConcurrentTasks, readdir, writeFile } from '../packages/shared/src';
-import { getPreviousFileContent } from './helpers';
+import { updateMarkdownContent } from './helpers';
 
 (async () => {
-  const packagesDir = path.resolve(__dirname, '../packages/');
-  const packages = await readdir(packagesDir);
-
-  await new ConcurrentTasks(
-    packages.map(packagePath => async () => {
-      const readmePath = path.resolve(packagesDir, packagePath, 'README.md');
-      const md = await getPreviousFileContent(readmePath);
-      if (!md) {
-        return;
-      }
-
-      await writeFile(readmePath, md);
-    }),
-    '',
-  ).run();
+  await updateMarkdownContent();
 })();
