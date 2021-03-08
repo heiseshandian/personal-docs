3f2531330f2f138c1b3c5aa7e0955a33c6599bf3
gqzhang5 <gqzhang5@iflytek.com>
2021-03-08 09:14:17 +0800

    feat: 更新master-dl
    affects: master-dl

--- packages/master-dl/src/lib/utils.ts
+++ packages/master-dl/src/lib/utils.ts
@@ -1,1 +1,5 @@
-export const sanitize = (str: string) => str.replace(/[^\w\s-._]/gi, '-');
+export const sanitize = (str: string) =>
+  str
+    .replace(/\b\)|\(\b|(,$)/gi, '')
+    .replace(/\b,/gi, ' -')
+    .replace(/[^\w\s-._]/gi, '-');
